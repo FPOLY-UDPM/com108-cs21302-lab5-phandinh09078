8b58f656ec789fcf1b8f4e361a4b6e8fa3a780c1
Helios1602 <phandinh09078@gmail.com>
2026-02-03 10:34:01 +0700

    LAB5

--- bai1.c
+++ bai1.c
@@ -1,29 +1,31 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Phan Nhu Dinh]
+ * MSSV:      [Ps49566]
+ * Lớp:       [COM108_CS21302]
  *****************************************************************************/
 
 //  BÀI 1: XÂY DỰNG CHƯƠNG TRÌNH TÌM GIÁ TRỊ LỚN NHẤT TRONG 3 SỐ  
 //  Input: Nhập từ bàn phím 3 số bất kỳ 
 //  Output: Xuất ra màn hình số lớn nhất trong 3 số 
 //  Hướng dẫn: Xây dựng hàm tìm giá trị lớn nhất trong 3 số. Hàm có giá trị trả về mà 
-//  giá trị lớn nhất giữa 3 số 
-
-#include <stdio.h>
-
-// //Tạo hàm 
-// int tenHam(so1, so2, so3){ 
-// int max; 
-// //Cách giải để tìm ra số lớn nhất 
-// return max; 
-// } 
+// #include <stdio.h>
 
 int main() {
+    int a, b, c;
+    int max;
 
-    // //Gọi hàm trong hàm main 
-    // //ví dụ 
-    // int soMax = temHam(3,5,10)
-    
+    printf("Nhập ba số nguyên: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    max = a;
+    if (b > max) {
+        max = b;
+    }
+    if (c > max) {
+        max = c;
+    }
+
+    printf("Giá trị lớn nhất là: %d\n", max);
+
     return 0;
 }
